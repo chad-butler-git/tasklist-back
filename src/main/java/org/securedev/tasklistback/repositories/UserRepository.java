5a29518fbbdf70484ab99ff18a1051fa9271fa49
Chad Butler <chad.butler@gmail.com>
2021-10-12 15:57:38 -0700

    added changes for SQL Injection testing

--- src/main/java/org/securedev/tasklistback/repositories/UserRepository.java
+++ src/main/java/org/securedev/tasklistback/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package org.securedev.tasklistback.repositories;
 
 import org.securedev.tasklistback.models.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
 
     User findByEmail(final String email);
 
